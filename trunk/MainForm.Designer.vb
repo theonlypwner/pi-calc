f8b75f71e7225d9f3c1d7d3029c65fe97d5c7cdb
theonlypwner <theonlypwner@a81760a6-80c8-1722-1cec-e1e6cacfddb3>
2010-07-05 21:09:41 +0000

    Option Strict compatible; Added separator; changed memory size naming method

--- trunk/MainForm.Designer.vb
+++ trunk/MainForm.Designer.vb
@@ -24,6 +24,7 @@ Partial Class MainForm
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container
         Me.MenuBar = New System.Windows.Forms.MenuStrip
+        Me.menuFile = New System.Windows.Forms.ToolStripMenuItem
         Me.lblCPUtxt = New System.Windows.Forms.Label
         Me.lblCPU = New System.Windows.Forms.Label
         Me.copyText = New System.Windows.Forms.ContextMenuStrip(Me.components)
@@ -32,26 +33,35 @@ Partial Class MainForm
         Me.lblOS = New System.Windows.Forms.Label
         Me.lblMemorytxt = New System.Windows.Forms.Label
         Me.lblMemory = New System.Windows.Forms.Label
+        Me.lblSep = New System.Windows.Forms.Label
+        Me.MenuBar.SuspendLayout()
         Me.copyText.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuBar
         '
+        Me.MenuBar.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuFile})
         Me.MenuBar.Location = New System.Drawing.Point(0, 0)
         Me.MenuBar.Name = "MenuBar"
-        Me.MenuBar.Padding = New System.Windows.Forms.Padding(9, 3, 0, 3)
-        Me.MenuBar.Size = New System.Drawing.Size(666, 24)
+        Me.MenuBar.Padding = New System.Windows.Forms.Padding(8, 2, 0, 2)
+        Me.MenuBar.Size = New System.Drawing.Size(552, 24)
         Me.MenuBar.TabIndex = 0
         Me.MenuBar.Text = "MenuStrip1"
+        '
+        'menuFile
+        '
+        Me.menuFile.Name = "menuFile"
+        Me.menuFile.Size = New System.Drawing.Size(37, 20)
+        Me.menuFile.Text = "File"
         '
         'lblCPUtxt
         '
         Me.lblCPUtxt.AutoSize = True
-        Me.lblCPUtxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblCPUtxt.Location = New System.Drawing.Point(39, 24)
+        Me.lblCPUtxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCPUtxt.Location = New System.Drawing.Point(30, 24)
         Me.lblCPUtxt.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblCPUtxt.Name = "lblCPUtxt"
-        Me.lblCPUtxt.Size = New System.Drawing.Size(55, 20)
+        Me.lblCPUtxt.Size = New System.Drawing.Size(47, 16)
         Me.lblCPUtxt.TabIndex = 1
         Me.lblCPUtxt.Text = "CPU: "
         Me.lblCPUtxt.TextAlign = System.Drawing.ContentAlignment.TopRight
@@ -60,10 +70,10 @@ Partial Class MainForm
         '
         Me.lblCPU.AutoSize = True
         Me.lblCPU.ContextMenuStrip = Me.copyText
-        Me.lblCPU.Location = New System.Drawing.Point(102, 24)
+        Me.lblCPU.Location = New System.Drawing.Point(85, 24)
         Me.lblCPU.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblCPU.Name = "lblCPU"
-        Me.lblCPU.Size = New System.Drawing.Size(476, 20)
+        Me.lblCPU.Size = New System.Drawing.Size(389, 16)
         Me.lblCPU.TabIndex = 1
         Me.lblCPU.Text = "Intel(R) Core(TM) i5 CPU750  @ 2.67GHz, 1.1v, 4 cores, 4 threads"
         '
@@ -82,11 +92,11 @@ Partial Class MainForm
         'lblOStxt
         '
         Me.lblOStxt.AutoSize = True
-        Me.lblOStxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblOStxt.Location = New System.Drawing.Point(50, 44)
+        Me.lblOStxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOStxt.Location = New System.Drawing.Point(40, 40)
         Me.lblOStxt.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblOStxt.Name = "lblOStxt"
-        Me.lblOStxt.Size = New System.Drawing.Size(44, 20)
+        Me.lblOStxt.Size = New System.Drawing.Size(37, 16)
         Me.lblOStxt.TabIndex = 1
         Me.lblOStxt.Text = "OS: "
         Me.lblOStxt.TextAlign = System.Drawing.ContentAlignment.TopRight
@@ -95,21 +105,21 @@ Partial Class MainForm
         '
         Me.lblOS.AutoSize = True
         Me.lblOS.ContextMenuStrip = Me.copyText
-        Me.lblOS.Location = New System.Drawing.Point(102, 44)
+        Me.lblOS.Location = New System.Drawing.Point(85, 40)
         Me.lblOS.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblOS.Name = "lblOS"
-        Me.lblOS.Size = New System.Drawing.Size(332, 20)
+        Me.lblOS.Size = New System.Drawing.Size(271, 16)
         Me.lblOS.TabIndex = 1
         Me.lblOS.Text = "Microsoft Windows 7 Ultimate  6.1.7600 64-bit"
         '
         'lblMemorytxt
         '
         Me.lblMemorytxt.AutoSize = True
-        Me.lblMemorytxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblMemorytxt.Location = New System.Drawing.Point(13, 64)
+        Me.lblMemorytxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMemorytxt.Location = New System.Drawing.Point(6, 56)
         Me.lblMemorytxt.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblMemorytxt.Name = "lblMemorytxt"
-        Me.lblMemorytxt.Size = New System.Drawing.Size(81, 20)
+        Me.lblMemorytxt.Size = New System.Drawing.Size(71, 16)
         Me.lblMemorytxt.TabIndex = 1
         Me.lblMemorytxt.Text = "Memory: "
         Me.lblMemorytxt.TextAlign = System.Drawing.ContentAlignment.TopRight
@@ -118,32 +128,45 @@ Partial Class MainForm
         '
         Me.lblMemory.AutoSize = True
         Me.lblMemory.ContextMenuStrip = Me.copyText
-        Me.lblMemory.Location = New System.Drawing.Point(102, 64)
+        Me.lblMemory.Location = New System.Drawing.Point(85, 56)
         Me.lblMemory.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblMemory.Name = "lblMemory"
-        Me.lblMemory.Size = New System.Drawing.Size(132, 20)
+        Me.lblMemory.Size = New System.Drawing.Size(106, 16)
         Me.lblMemory.TabIndex = 1
         Me.lblMemory.Text = "DDR3-1333 8GB"
         '
+        'lblSep
+        '
+        Me.lblSep.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblSep.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSep.Location = New System.Drawing.Point(0, 78)
+        Me.lblSep.Name = "lblSep"
+        Me.lblSep.Size = New System.Drawing.Size(557, 2)
+        Me.lblSep.TabIndex = 2
+        '
         'MainForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(666, 238)
+        Me.ClientSize = New System.Drawing.Size(552, 180)
+        Me.Controls.Add(Me.lblSep)
         Me.Controls.Add(Me.lblMemorytxt)
         Me.Controls.Add(Me.lblOS)
-        Me.Controls.Add(Me.lblMemory)
         Me.Controls.Add(Me.MenuBar)
-        Me.Controls.Add(Me.lblCPU)
         Me.Controls.Add(Me.lblCPUtxt)
+        Me.Controls.Add(Me.lblMemory)
         Me.Controls.Add(Me.lblOStxt)
-        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Controls.Add(Me.lblCPU)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MainMenuStrip = Me.MenuBar
-        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.MaximizeBox = False
         Me.Name = "MainForm"
         Me.Text = "Pi Calculator"
+        Me.MenuBar.ResumeLayout(False)
+        Me.MenuBar.PerformLayout()
         Me.copyText.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -158,5 +181,7 @@ Partial Class MainForm
     Friend WithEvents lblOS As System.Windows.Forms.Label
     Friend WithEvents lblMemorytxt As System.Windows.Forms.Label
     Friend WithEvents lblMemory As System.Windows.Forms.Label
+    Friend WithEvents menuFile As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lblSep As System.Windows.Forms.Label
 
 End Class
--- trunk/MainForm.vb
+++ trunk/MainForm.vb
@@ -7,10 +7,10 @@
         Dim i, i2 As Long ' Long Is Int64
         Dim s As String = Nothing
         For Each cpu As ManagementObject In p.Get() ' there could be more than one processor, like a server?
-            i += cpu("NumberOfCores") ' counts cores ' reminds me when I used i += 1, "why doesn't VB have i++ like c++?"
-            i2 += cpu("NumberOfLogicalProcessors") ' counts threads
+            i += CInt(cpu("NumberOfCores")) ' counts cores ' reminds me when I used i += 1, "why doesn't VB have i++ like c++?"
+            i2 += CInt(cpu("NumberOfLogicalProcessors")) ' counts threads
             If s Is Nothing Then ' CPU name
-                s = cpu("Name").Replace("   ", "") & ", " & (cpu("CurrentVoltage") / 10) & "v"
+                s = cpu("Name").ToString.Replace("   ", "") & ", " & (CInt(cpu("CurrentVoltage")) / 10) & "v"
             End If
         Next
         lblCPU.Text = s & ", " & i & " cores, " & i2 & " threads"
@@ -18,7 +18,7 @@
         q = New ObjectQuery("SELECT * FROM Win32_OperatingSystem")
         p = New ManagementObjectSearcher(q)
         For Each os As ManagementObject In p.Get()
-            lblOS.Text = os("Caption") & " " & os("Version") & " " & os("OSArchitecture")
+            lblOS.Text = CStr(os("Caption")) & " " & CStr(os("Version")) & " " & CStr(os("OSArchitecture"))
             Exit For ' stupid collection doesn't let me do p.Get()[0]
         Next
         ' Memory Information
@@ -27,9 +27,9 @@
         i = 0
         s = Nothing
         For Each mm As ManagementObject In p.Get()
-            i += mm("Capacity") ' add the memory module's capacity
+            i += CLng(mm("Capacity")) ' add the memory module's capacity
             If s Is Nothing Then ' describe the memory
-                Select Case mm("MemoryType") ' Covers DDR and DDR2, others and assumes DDR3
+                Select Case CShort(mm("MemoryType")) ' Covers DDR and DDR2, others and assumes DDR3
                     Case 1 To 19 ' not DDR memory
                         s = "Unknown"
                     Case 20
@@ -39,16 +39,15 @@
                     Case Else ' Assume unknown memory (Case 0) to be DDR3
                         s = "DDR3"
                 End Select
-                s &= "-" & mm("Speed")
+                s &= "-" & CStr(mm("Speed"))
             End If
         Next
         Dim sf As Char() = {"K"c, "M"c, "G"c}
         i2 = 0
-        For Each c As Char In sf
-            If i / (1024 ^ i2) < 1024 Then Exit For ' don't reduce it again
+        Do Until (i / (1024 ^ i2) < 1024) Or i2 > sf.Length
             i2 += 1
-        Next
-        lblMemory.Text = s & " " & (i / (1024 ^ i2)) & If(i2 > 0, sf(i2 - 1), "") & "B"
+        Loop
+        lblMemory.Text = s & " " & (i / (1024 ^ i2)) & If(i2 > 0, sf(CInt(i2 - 1)), "") & "B"
     End Sub
 
     Private Sub copyTextBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles copyTextBtn.Click
