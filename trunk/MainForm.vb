4958ee980f245d22ca657158ac27032af866425e
theonlypwner <theonlypwner@a81760a6-80c8-1722-1cec-e1e6cacfddb3>
2010-07-05 00:20:42 +0000

    Memory Recognition - Assumes unknown memory types (in the future, DDR4) to be DDR3

--- trunk/MainForm.vb
+++ trunk/MainForm.vb
@@ -4,7 +4,7 @@
         ' CPU Information
         Dim q As New ObjectQuery("SELECT * FROM Win32_Processor")
         Dim p As New ManagementObjectSearcher(q)
-        Dim i, i2 As Integer
+        Dim i, i2 As Long ' Long Is Int64
         Dim s As String = Nothing
         For Each cpu As ManagementObject In p.Get() ' there could be more than one processor, like a server?
             i += cpu("NumberOfCores") ' counts cores ' reminds me when I used i += 1, "why doesn't VB have i++ like c++?"
@@ -21,6 +21,33 @@
             lblOS.Text = os("Caption") & " " & os("Version") & " " & os("OSArchitecture")
             Exit For ' stupid collection doesn't let me do p.Get()[0]
         Next
+        ' Memory Information
+        q = New ObjectQuery("SELECT * FROM Win32_PhysicalMemory")
+        p = New ManagementObjectSearcher(q)
+        i = 0
+        s = Nothing
+        For Each mm As ManagementObject In p.Get()
+            i += mm("Capacity") ' add the memory module's capacity
+            If s Is Nothing Then ' describe the memory
+                Select Case mm("MemoryType") ' Covers DDR and DDR2, others and assumes DDR3
+                    Case 1 To 19 ' not DDR memory
+                        s = "Unknown"
+                    Case 20
+                        s = "DDR"
+                    Case 21
+                        s = "DDR2"
+                    Case Else ' Assume unknown memory (Case 0) to be DDR3
+                        s = "DDR3"
+                End Select
+            End If
+        Next
+        Dim sf As Char() = {"K"c, "M"c, "G"c}
+        i2 = 0
+        For Each c As Char In sf
+            If i / (1024 ^ i2) < 1024 Then Exit For ' don't reduce it again
+            i2 += 1
+        Next
+        lblMemory.Text = s & " " & (i / (1024 ^ i2)) & If(i2 > 0, sf(i2 - 1), "") & "B"
     End Sub
 
     Private Sub copyTextBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles copyTextBtn.Click
