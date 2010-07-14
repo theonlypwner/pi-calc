db3376850b8be53621979cec07216d12d7c36c7c
theonlypwner <theonlypwner@a81760a6-80c8-1722-1cec-e1e6cacfddb3>
2010-07-14 03:45:38 +0000

    Still in development; 4 spaces -> tab; Algorithm not implemented; Only first two digits are correct, no calculation is made except for addition and memory write

--- CalculatePi.vb
+++ CalculatePi.vb
@@ -1,38 +1,83 @@
-﻿''' <summary>
-''' Responsible for calculating pi
-''' </summary>
+﻿''' <summary>Responsible for calculating pi</summary>
 Public Class CalculatePi
-    ''' <summary>How many digits of pi to compute</summary>
-    Public precision As UInteger
-    ''' <summary>If data storage is needed</summary>
-    Protected store As Boolean
-    ''' <summary>temp unsigned 16-bit integer</summary>
-    Private i As UInt16
-    ''' <summary>Contains the results</summary>
-    Protected result As ArrayList
+	''' <summary>CR &amp; LF = Windows New Line</summary>
+	Public Const CrLf As String = Chr(13) & Chr(10) ' CR & LF = Windows New Line
 
-    ''' <summary>Raised upon thread completion</summary>
-    Public Event onComplete(ByVal sender As System.Object, ByVal e As System.EventArgs)
-    ''' <summary>Raised upon progress update</summary>
-    Public Event onProgress(ByVal p As UInteger)
+	''' <summary>How many digits of pi to compute</summary>
+	Public precision As UShort
+	''' <summary>If the result buffer should be filled</summary>
+	Protected store As Boolean
+	''' <summary>temp unsigned 16-bit integer</summary>
+	Private i As UInt16
+	''' <summary>Contains the results</summary>
+	Protected result As ArrayList
 
-    ''' <param name="p">Precision to calculate pi</param>
-    ''' <param name="s">If calculation should be stored</param>
-    Public Sub New(ByVal p As Integer, ByVal s As Boolean)
-        precision = p
-        store = s
-    End Sub
+	''' <summary>Raised upon thread completion</summary>
+	Public Event onComplete(ByVal sender As System.Object, ByVal e As System.EventArgs)
+	''' <summary>Raised upon progress update</summary>
+	Public Event onProgress(ByVal p As UShort)
 
-    ''' <summary>The Thread instance calls this function</summary>
-    Public Sub Process()
-        Do
-            i += 1
-            If i > precision Then
-                RaiseEvent onComplete(Me, Nothing)
-                Exit Sub
-            End If
-            RaiseEvent onProgress(i)
-            Thread.CurrentThread.Sleep(10)
-        Loop
-    End Sub
+	''' <param name="p">Precision to calculate pi</param>
+	''' <param name="s">Store in memory or not</param>
+	Public Sub New(ByVal p As UShort, ByVal s As Boolean)
+		precision = p
+		store = s
+		result = New ArrayList()
+	End Sub
+
+	''' <summary>The Thread instance calls this function</summary>
+	Protected Friend Sub Process()
+		Do
+			i = CUShort(i + 1)
+			If i > precision Then
+				RaiseEvent onComplete(Me, Nothing)
+				Exit Sub
+			End If
+			If store Then result.Add(i Mod 10)
+			RaiseEvent onProgress(i)
+			' Thread.CurrentThread.Sleep(ms)
+		Loop
+	End Sub
+
+	Public Enum ResultType
+		None = 0
+		First2000 = 1
+		All = 2
+	End Enum
+
+	''' <summary>Holds a result string with start timestamp and difference to finish</summary>
+	Public Structure TimedResult
+		Public s As String
+		Public timeStart As Long
+		Public timeDiff As TimeSpan
+	End Structure
+
+	Protected Friend Function ResultDataNoDelete(Optional ByVal n As ResultType = ResultType.None) As TimedResult
+		Dim ret As TimedResult
+		ret.timeStart = Now.Ticks
+		If (Not store) Or result.Count < 1 Then
+			ret.s = "No buffer"
+			Return ret
+		ElseIf n = ResultType.None Then
+			ret.s = "Buffer not displayed"
+			Return ret
+		End If
+		ret.s = "3."
+		If n = ResultType.First2000 And precision > 2000 Then
+			ret.s = "Result is trimmed" & CrLf & "3."
+		End If
+		Dim j As Object() = result.ToArray
+		For i As UShort = 0 To If(n = ResultType.First2000 And precision > 2000, Math.Min(j.Length, 2000), j.Length) - 1
+			ret.s &= CStr(j(i))
+		Next
+		Return ret
+	End Function
+
+	Protected Friend ReadOnly Property ResultData(Optional ByVal n As ResultType = ResultType.All) As TimedResult
+		Get
+			Dim r = Me.ResultDataNoDelete(n)
+			result = Nothing
+			Return r
+		End Get
+	End Property
 End Class
--- MainForm.vb
+++ MainForm.vb
@@ -1,214 +1,219 @@
 ﻿Public Class MainForm
 
-    Protected Friend t As Thread
-    Protected WithEvents piCalc As CalculatePi
-    Public Const CrLf As String = Chr(13) & Chr(10) ' CR & LF = Windows New Line
+	Protected Friend t As Thread
+	Protected WithEvents piCalc As CalculatePi
+	''' <summary>CR &amp; LF = Windows New Line</summary>
+	Public Const CrLf As String = Chr(13) & Chr(10)
 
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        ' CPU Information
-        Dim q As New ObjectQuery("SELECT * FROM Win32_Processor")
-        Dim p As New ManagementObjectSearcher(q)
-        Dim i, i2 As Long ' Long Is Int64
-        Dim s As String = Nothing
-        For Each cpu As ManagementObject In p.Get() ' there could be more than one processor, like a server?
-            i += CInt(cpu("NumberOfCores")) ' counts cores ' reminds me when I used i += 1, "why doesn't VB have i++ like c++?"
-            i2 += CInt(cpu("NumberOfLogicalProcessors")) ' counts threads
-            If s Is Nothing Then ' CPU name
-                s = cpu("Name").ToString.Replace("   ", "") & ", " & (CInt(cpu("CurrentVoltage")) / 10) & "v"
-            End If
-        Next
-        lblCPU.Text = s & ", " & i & " cores, " & i2 & " threads"
-        ' OS Information
-        q = New ObjectQuery("SELECT * FROM Win32_OperatingSystem")
-        p = New ManagementObjectSearcher(q)
-        For Each os As ManagementObject In p.Get()
-            lblOS.Text = CStr(os("Caption")) & " " & CStr(os("Version")) & " " & CStr(os("OSArchitecture"))
-            Exit For ' stupid collection doesn't let me do p.Get()[0]
-        Next
-        ' Memory Information
-        q = New ObjectQuery("SELECT * FROM Win32_PhysicalMemory")
-        p = New ManagementObjectSearcher(q)
-        i = 0
-        s = Nothing
-        For Each mm As ManagementObject In p.Get()
-            i += CLng(mm("Capacity")) ' add the memory module's capacity
-            If s Is Nothing Then ' describe the memory
-                Select Case CShort(mm("MemoryType")) ' Covers DDR and DDR2, others and assumes DDR3
-                    Case 1 To 19 ' not DDR memory
-                        s = "Unknown"
-                    Case 20
-                        s = "DDR"
-                    Case 21
-                        s = "DDR2"
-                    Case Else ' Assume unknown memory (Case 0) to be DDR3
-                        s = "DDR3"
-                End Select
-                s &= "-" & CStr(mm("Speed"))
-            End If
-        Next
-        Dim sf As Char() = {"K"c, "M"c, "G"c}
-        i2 = 0
-        Do Until (i / (1024 ^ i2) < 1024) Or i2 > sf.Length
-            i2 += 1
-        Loop
-        lblMemory.Text = s & " " & (i / (1024 ^ i2)) & If(i2 > 0, sf(CInt(i2 - 1)), "") & "B"
-        i2 = 0 ' now counting elements to pop
-        Select Case i ' bytes of memory
-            Case Is < 256 * 1024 ^ 2 ' You should have at least 256 MB of RAM
-                Me.Close() ' should not use this program
-            Case 256 * 1024 ^ 2 To 512 * 1024 ^ 2 ' 256MB to 512MB
-                i2 = 5 ' not suitable for more than 16M
-        End Select
-        For i = 1 To i2
-            MsgBox("rem")
-            cmbPrecision.Items.RemoveAt(cmbPrecision.Items.Count - 1)
-        Next
-        If i2 > 0 Then ' popped elements, update maximum
-            maxValChanged(Me, Nothing)
-        End If
-        ' ComboBox Initalization - Visual Studio doesn't have these important properties available for ComboBox controls
-        cmbPrecision.SelectedIndex = 6
-        cmbDScale.SelectedIndex = 0
-        cmbBuffer.SelectedIndex = 2
-        ' Store progress size difference
-        progress.Tag = Me.Width - progress.Width
-    End Sub
+	Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+		' CPU Information
+		Dim q As New ObjectQuery("SELECT * FROM Win32_Processor")
+		Dim p As New ManagementObjectSearcher(q)
+		Dim i, i2 As Long ' Long Is Int64
+		Dim s As String = Nothing
+		For Each cpu As ManagementObject In p.Get() ' there could be more than one processor, like a server?
+			i += CInt(cpu("NumberOfCores")) ' counts cores ' reminds me when I used i += 1, "why doesn't VB have i++ like c++?"
+			i2 += CInt(cpu("NumberOfLogicalProcessors")) ' counts threads
+			If s Is Nothing Then ' CPU name
+				s = cpu("Name").ToString.Replace("   ", "") & ", " & (CInt(cpu("CurrentVoltage")) / 10) & "v"
+			End If
+		Next
+		lblCPU.Text = s & ", " & i & " cores, " & i2 & " threads"
+		' OS Information
+		q = New ObjectQuery("SELECT * FROM Win32_OperatingSystem")
+		p = New ManagementObjectSearcher(q)
+		For Each os As ManagementObject In p.Get()
+			lblOS.Text = CStr(os("Caption")) & " " & CStr(os("Version")) & " " & CStr(os("OSArchitecture"))
+			Exit For ' stupid collection doesn't let me do p.Get()[0]
+		Next
+		' Memory Information
+		q = New ObjectQuery("SELECT * FROM Win32_PhysicalMemory")
+		p = New ManagementObjectSearcher(q)
+		i = 0
+		s = Nothing
+		For Each mm As ManagementObject In p.Get()
+			i += CLng(mm("Capacity")) ' add the memory module's capacity
+			If s Is Nothing Then ' describe the memory
+				Select Case CShort(mm("MemoryType")) ' Covers DDR and DDR2, others and assumes DDR3
+					Case 1 To 19 ' not DDR memory
+						s = "Unknown"
+					Case 20
+						s = "DDR"
+					Case 21
+						s = "DDR2"
+					Case Else ' Assume unknown memory (Case 0) to be DDR3
+						s = "DDR3"
+				End Select
+				s &= "-" & CStr(mm("Speed"))
+			End If
+		Next
+		Dim sf As Char() = {"K"c, "M"c, "G"c}
+		i2 = 0
+		Do Until (i / (1024 ^ i2) < 1024) Or i2 > sf.Length
+			i2 += 1
+		Loop
+		lblMemory.Text = s & " " & (i / (1024 ^ i2)) & If(i2 > 0, sf(CInt(i2 - 1)), "") & "B"
+		i2 = 0 ' now counting elements to pop
+		Select Case i ' bytes of memory
+			Case Is < 268435456 ' You should have at least 256 MB of RAM
+				Me.Close() ' should not use this program
+			Case 268435456 To 536870912 ' 256MB to 512MB
+				i2 = 5 ' not suitable for more than 16M
+		End Select
+		For i = 1 To i2
+			MsgBox("rem")
+			cmbPrecision.Items.RemoveAt(cmbPrecision.Items.Count - 1)
+		Next
+		If i2 > 0 Then ' popped elements, update maximum
+			maxValChanged(Me, Nothing)
+		End If
+		' ComboBox Initalization - Visual Studio doesn't have these important properties available for ComboBox controls
+		cmbPrecision.SelectedIndex = 6
+		cmbDScale.SelectedIndex = 0
+		cmbBuffer.SelectedIndex = 2
+		' Store progress size difference
+		progress.Tag = Me.Width - progress.Width
+	End Sub
 
-    ' Fix for the size of the progressbar in the status bar
-    Private Sub MainForm_SizeChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.SizeChanged
-        If IsNumeric(progress.Tag) Then progress.Size = New Drawing.Size(Me.Width - CInt(progress.Tag), progress.Height)
-    End Sub
+	' Fix for the size of the progressbar in the status bar
+	Private Sub MainForm_SizeChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.SizeChanged
+		If IsNumeric(progress.Tag) Then progress.Size = New Drawing.Size(Me.Width - CInt(progress.Tag), progress.Height)
+	End Sub
 
-    Private Sub copyTextBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles copyTextBtn.Click
-        ' Copy the static label text
-        Clipboard.SetText(CType(CType(CType(sender, ToolStripMenuItem).Owner, ContextMenuStrip).SourceControl, Control).Text)
-    End Sub
+	Private Sub copyTextBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles copyTextBtn.Click
+		' Copy the static label text
+		Clipboard.SetText(CType(CType(CType(sender, ToolStripMenuItem).Owner, ContextMenuStrip).SourceControl, Control).Text)
+	End Sub
 
-    Private Sub numPrecision_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles numPrecision.ValueChanged
-        If cmbDScale.SelectedIndex = 0 Then ' IndexOf Note: [compare not] 0 because -1 is not there, 0 is first char
-            ' 1024s
-            If numPrecision.Value Mod 1048576 = 0 Then
-                ' M
-                Dim j As UInteger = numPrecision.Value / 1048576
-                For Each i As String In cmbPrecision.Items
-                    If i.IndexOf("M"c) > 0 And i.Replace("M", "") = CStr(j) Then
-                        cmbPrecision.SelectedItem = i
-                        Exit Sub
-                    End If
-                Next
-            End If
-            If numPrecision.Value Mod 1024 = 0 Then
-                ' K
-                Dim j As UInteger = numPrecision.Value / 1024
-                For Each i As String In cmbPrecision.Items
-                    If i.IndexOf("K"c) > 0 And i.Replace("K", "") = CStr(j) Then
-                        cmbPrecision.SelectedItem = i
-                        Exit Sub
-                    End If
-                Next
-            End If
-        Else
-            ' 1000s
-            If numPrecision.Value Mod 1000000 = 0 Then
-                ' m
-                Dim j As UInteger = numPrecision.Value / 1000000
-                For Each i As String In cmbPrecision.Items
-                    If i.IndexOf("M"c) > 0 And i.Replace("M", "") = CStr(j) Then
-                        cmbPrecision.SelectedItem = i
-                        Exit Sub
-                    End If
-                Next
-            ElseIf numPrecision.Value Mod 1000 = 0 Then
-                ' k
-                Dim j As UInteger = numPrecision.Value / 1000
-                For Each i As String In cmbPrecision.Items
-                    If i.IndexOf("K"c) > 0 And i.Replace("K", "") = CStr(j) Then
-                        cmbPrecision.SelectedItem = i
-                        Exit Sub
-                    End If
-                Next
-            End If
-        End If
-        For Each i As String In cmbPrecision.Items
-            If i.IndexOf("K"c) < 0 And i.IndexOf("M"c) < 0 And i = CStr(numPrecision.Value) Then
-                cmbPrecision.SelectedItem = i
-                Exit Sub
-            End If
-        Next
-        cmbPrecision.SelectedItem = "?"
-    End Sub
+	Private Sub numPrecision_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles numPrecision.ValueChanged
+		If cmbDScale.SelectedIndex = 0 Then ' IndexOf Note: [compare not] 0 because -1 is not there, 0 is first char
+			' 1024s
+			If numPrecision.Value Mod 1048576 = 0 Then
+				' M
+				Dim j As UInteger = CUInt(numPrecision.Value / 1048576)
+				For Each i As String In cmbPrecision.Items
+					If i.IndexOf("M"c) > 0 And i.Replace("M", "") = CStr(j) Then
+						cmbPrecision.SelectedItem = i
+						Exit Sub
+					End If
+				Next
+			End If
+			If numPrecision.Value Mod 1024 = 0 Then
+				' K
+				Dim j As UInteger = CUInt(numPrecision.Value / 1024)
+				For Each i As String In cmbPrecision.Items
+					If i.IndexOf("K"c) > 0 And i.Replace("K", "") = CStr(j) Then
+						cmbPrecision.SelectedItem = i
+						Exit Sub
+					End If
+				Next
+			End If
+		Else
+			' 1000s
+			If numPrecision.Value Mod 1000000 = 0 Then
+				' m
+				Dim j As UInteger = CUInt(numPrecision.Value / 1000000)
+				For Each i As String In cmbPrecision.Items
+					If i.IndexOf("M"c) > 0 And i.Replace("M", "") = CStr(j) Then
+						cmbPrecision.SelectedItem = i
+						Exit Sub
+					End If
+				Next
+			ElseIf numPrecision.Value Mod 1000 = 0 Then
+				' k
+				Dim j As UInteger = CUInt(numPrecision.Value / 1000)
+				For Each i As String In cmbPrecision.Items
+					If i.IndexOf("K"c) > 0 And i.Replace("K", "") = CStr(j) Then
+						cmbPrecision.SelectedItem = i
+						Exit Sub
+					End If
+				Next
+			End If
+		End If
+		For Each i As String In cmbPrecision.Items
+			If i.IndexOf("K"c) < 0 And i.IndexOf("M"c) < 0 And i = CStr(numPrecision.Value) Then
+				cmbPrecision.SelectedItem = i
+				Exit Sub
+			End If
+		Next
+		cmbPrecision.SelectedItem = "?"
+	End Sub
 
-    Private Sub precisionComboChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbPrecision.SelectedIndexChanged, cmbDScale.SelectedIndexChanged
-        If cmbPrecision.SelectedIndex = 0 Then
-            numPrecision_ValueChanged(sender, New EventArgs)
-        ElseIf CStr(cmbPrecision.SelectedItem).IndexOf("M"c) > 0 Then
-            numPrecision.Value = CInt(CStr(cmbPrecision.SelectedItem).Replace("M", "")) * If(cmbDScale.SelectedIndex = 0, 1048576, 1000000)
-        ElseIf CStr(cmbPrecision.SelectedItem).IndexOf("K"c) > 0 Then
-            numPrecision.Value = CInt(CStr(cmbPrecision.SelectedItem).Replace("K", "")) * If(cmbDScale.SelectedIndex = 0, 1024, 1000)
-        Else
-            numPrecision.Value = CInt(cmbPrecision.SelectedItem)
-        End If
-    End Sub
+	Private Sub precisionComboChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbPrecision.SelectedIndexChanged, cmbDScale.SelectedIndexChanged
+		If cmbPrecision.SelectedIndex = 0 Then
+			numPrecision_ValueChanged(sender, New EventArgs)
+		ElseIf CStr(cmbPrecision.SelectedItem).IndexOf("M"c) > 0 Then
+			numPrecision.Value = CInt(CStr(cmbPrecision.SelectedItem).Replace("M", "")) * If(cmbDScale.SelectedIndex = 0, 1048576, 1000000)
+		ElseIf CStr(cmbPrecision.SelectedItem).IndexOf("K"c) > 0 Then
+			numPrecision.Value = CInt(CStr(cmbPrecision.SelectedItem).Replace("K", "")) * If(cmbDScale.SelectedIndex = 0, 1024, 1000)
+		Else
+			numPrecision.Value = CInt(cmbPrecision.SelectedItem)
+		End If
+	End Sub
 
-    Private Sub maxValChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbDScale.SelectedIndexChanged
-        Dim l As String = CStr(cmbPrecision.Items(cmbPrecision.Items.Count - 1))
-        If l.IndexOf("M"c) > 0 Then
-            l = l.Replace("M", "")
-            l = CStr(CInt(l) * If(cmbDScale.SelectedIndex = 0, 1048576, 1000000))
-        ElseIf l.IndexOf("K"c) > 0 Then
-            l = l.Replace("K", "")
-            l = CStr(CInt(l) * If(cmbDScale.SelectedIndex = 0, 1024, 1000))
-        End If
-        numPrecision.Maximum = CDec(l)
-    End Sub
+	Private Sub maxValChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbDScale.SelectedIndexChanged
+		Dim l As String = CStr(cmbPrecision.Items(cmbPrecision.Items.Count - 1))
+		If l.IndexOf("M"c) > 0 Then
+			l = l.Replace("M", "")
+			l = CStr(CInt(l) * If(cmbDScale.SelectedIndex = 0, 1048576, 1000000))
+		ElseIf l.IndexOf("K"c) > 0 Then
+			l = l.Replace("K", "")
+			l = CStr(CInt(l) * If(cmbDScale.SelectedIndex = 0, 1024, 1000))
+		End If
+		numPrecision.Maximum = CDec(l)
+	End Sub
 
-    Private Sub btnGo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGo.Click
-        ' update GUI to allow stop
-        btnGo.Enabled = False
-        btnStop.Enabled = True
-        ' lock GUI
-        numPrecision.Enabled = False
-        cmbDScale.Enabled = False
-        cmbPrecision.Enabled = False
-        ' update progress bar
-        progress.Maximum = numPrecision.Value
-        ' start thread
-        piCalc = New CalculatePi(numPrecision.Value, cmbBuffer.SelectedIndex > 0)
-        t = New Thread(AddressOf piCalc.Process)
-        t.Start()
-    End Sub
+	Private Sub btnGo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGo.Click
+		' update GUI to allow stop
+		btnGo.Enabled = False
+		btnStop.Enabled = True
+		' lock GUI
+		numPrecision.Enabled = False
+		cmbDScale.Enabled = False
+		cmbPrecision.Enabled = False
+		' update progress bar
+		progress.Maximum = CInt(numPrecision.Value)
+		' start thread
+		piCalc = New CalculatePi(CUShort(numPrecision.Value), Not cmbBuffer.SelectedIndex = 0)
+		t = New Thread(AddressOf piCalc.Process)
+		t.Start()
+	End Sub
 
-    Public Delegate Sub controlEventInvoker(ByVal sender As System.Object, ByVal e As System.EventArgs)
+	Public Delegate Sub controlEventInvoker(ByVal sender As System.Object, ByVal e As System.EventArgs)
 
-    Protected Friend Sub stopThread(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStop.Click, piCalc.onComplete
-        If Me.InvokeRequired Then
-            Me.Invoke(New controlEventInvoker(AddressOf stopThread), sender, e)
-            Exit Sub
-        End If
-        ' update GUI to allow start
-        btnGo.Enabled = True
-        btnStop.Enabled = False
-        ' unlock GUI
-        numPrecision.Enabled = True
-        cmbDScale.Enabled = True
-        cmbPrecision.Enabled = True
-        ' fill progress bar
-        progress.Value = 1
-        progress.Maximum = 1
-        ' delete thread
-        t.Abort() ' stop thread before delete
-        t = Nothing ' delete the instance, .NET will reclaim the memory for me
-        ' retrieve data and delete piCalc
-        txtResult.Text = If(sender Is btnStop, "Calculation was stopped" & CrLf, "")
-        piCalc = Nothing
-    End Sub
+	Protected Friend Sub stopThread(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStop.Click, piCalc.onComplete
+		If Me.InvokeRequired Then
+			Me.Invoke(New controlEventInvoker(AddressOf stopThread), sender, e)
+			Exit Sub
+		End If
+		' update GUI to allow start
+		btnGo.Enabled = True
+		btnStop.Enabled = False
+		' unlock GUI
+		numPrecision.Enabled = True
+		cmbDScale.Enabled = True
+		cmbPrecision.Enabled = True
+		' fill progress bar
+		progress.Value = 1
+		progress.Maximum = 1
+		' delete thread
+		If t.IsAlive Then t.Abort() ' stop thread before delete
+		t = Nothing ' delete the instance, .NET will reclaim the memory for me
+		' retrieve data and delete piCalc
+		Dim r As CalculatePi.TimedResult = piCalc.ResultDataNoDelete(If(cmbBuffer.SelectedIndex > 1, CalculatePi.ResultType.First2000, CalculatePi.ResultType.None))
+		txtResult.Text = If(sender Is btnStop, "Calculation was stopped" & CrLf, "") & "NOT FINISHED" & CrLf & r.s
+		r.timeDiff = New TimeSpan(Now.Ticks - r.timeStart)
+		' [debug] msgbox
+		lblDisplay.Text = r.timeDiff.Seconds.ToString.PadLeft(2, "0"c) & "s " & r.timeDiff.Milliseconds.ToString.PadLeft(3, "0"c) & "ms " & CStr(Math.Round(r.timeDiff.Ticks * 10) Mod 1000).PadLeft(3, "0"c) & "µs"
+		piCalc = Nothing
+	End Sub
 
-    Public Delegate Sub oneParamInvoker(ByVal i)
+	Public Delegate Sub oneParamInvoker(ByVal i As Object)
 
-    Protected Friend Sub calcProgress(ByVal p As UInteger) Handles piCalc.onProgress
-        If Me.InvokeRequired Then
-            Me.Invoke(New oneParamInvoker(AddressOf calcProgress), p)
-        End If
-        progress.Value = p
-    End Sub
+	Protected Friend Sub calcProgress(ByVal p As UShort) Handles piCalc.onProgress
+		If Me.InvokeRequired Then
+			Me.Invoke(New oneParamInvoker(AddressOf calcProgress), p)
+		End If
+		progress.Value = CInt(p)
+	End Sub
 End Class
