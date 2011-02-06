bdf3cc488867fb61f7d85cf94ab48f088c63f793
theonlypwner <theonlypwner@a81760a6-80c8-1722-1cec-e1e6cacfddb3>
2011-02-06 01:05:11 +0000

    BigDec development started

---
+++ BigDec.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using java.math;
+
+namespace Pi
+{
+	class BigDec : BigDecimal
+	{
+		public BigDec() : base(0) { }
+
+		// class definition
+		public static implicit operator BigDec(int i)
+		{
+			BigDecimal r = new BigDecimal(i);
+			return (BigDec)r;
+		}
+
+		public static implicit operator BigDec(double d) {
+			BigDecimal r = new BigDecimal(d);
+			return (BigDec)r;
+		}
+
+		public bool Equals(BigDec d) { return this == d; }
+		public override bool Equals(object val) { return val is BigDec && this == (BigDec)val; }
+		public override int GetHashCode() { return intValue() ^ scale(); }
+
+		// comparison operators
+		public static bool operator ==(BigDec a, BigDec b){ return a.compareTo(b) == 0; }
+		public static bool operator !=(BigDec a, BigDec b) { return !(a == b); }
+
+		public static bool operator >(BigDec a, BigDec b) { return a.compareTo(b) == 1; }
+		public static bool operator <(BigDec a, BigDec b) { return a.compareTo(b) == -1; }
+		public static bool operator >=(BigDec a, BigDec b) { return a > b || a == b; }
+		public static bool operator <=(BigDec a, BigDec b) { return a < b || a == b; }
+
+		// arithmetic operators
+		public static BigDec operator -(BigDec a){
+			BigDec result = a;
+			result.negate();
+			return result;
+		}
+		public static BigDec operator +(BigDec a, BigDec b) {
+			BigDec result = a;
+			result.add(b);
+			return result;
+		}
+		public static BigDec operator -(BigDec a, BigDec b)
+		{
+			BigDec result = a;
+			result.subtract(b);
+			return result;
+		}
+
+		// factorial
+		public static BigDec Factorial(ulong i)
+		{
+			return 1;
+		}
+	}
+}
---
+++ CalculatePi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pi
+{
+	class CalculatePi
+	{
+		public int precision;
+		public CalculatePi(int p) {
+			precision = p + 3;
+		}
+		BigDec result, buffer;
+		void process() {
+			int limit = (int)Math.Ceiling(precision / 14d - 1d);
+			result = 0;
+			buffer = 0;
+			for (int i = 0; i < limit; i++) {
+				buffer = 0;
+			}
+			/*
+		result = bcdiv(1, (bcmul(12, (result))), precision)
+		RaiseEvent onComplete(Me, New EventArgs)
+			 */
+		}
+	}
+}
